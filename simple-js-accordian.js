fdf92bcf2e93433658fbc9a0d0ed4c41c772f6a5
Joshua D Berkowitz <info@joshuaberkowitz.us>
2019-01-07 08:03:56 -0500

    Initial

---
+++ simple-js-accordian.js
@@ -0,0 +1,25 @@
+/*
+* @param clickSelector @type css class - the class of the item to be used as the accordian button
+* @param contentSelector @type css class - the class of the content to show when the selector is clicked.
+* @param classToToggle @type css class - the class to add or remove based upon the toggle. 
+* will add display:none to hide and display:block to show
+* Hides or shows teh next element sibling of the selected class
+*/
+
+
+function accordian(clickSelector, classToToggle){
+    var accordian = document.getElementsByClassName(clickSelector);    
+    var accs;            
+    for (accs = 0; accs < accordian.length; accs++) {
+        accordian[accs].addEventListener("click", function() {
+            this.classList.toggle(classToToggle);
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "none") {
+                panel.style.display = "block";
+            } else {
+                panel.style.display = "none";
+            };
+        });
+    };
+
+};    
